da52083340825215a11c0e29b9f4279c644b8cb7
yaoyongqi <113654761+yaoyongqi@users.noreply.github.com>
2022-09-19 18:29:45 +0800

    pull  commit

--- src/main/java/com/gittest/GitTest.java
+++ src/main/java/com/gittest/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("master");
         System.out.println("hot fix");
         System.out.println("push");
+        System.out.println("pull");
     }
 }
