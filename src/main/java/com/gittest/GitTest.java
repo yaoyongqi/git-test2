233dc2c5fe5fa4c8a7063bf9e588ceab179ad5d0
yyq <2903249079@qq.com>
2022-09-19 18:22:06 +0800

    push

--- src/main/java/com/gittest/GitTest.java
+++ src/main/java/com/gittest/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("444");
         System.out.println("master");
         System.out.println("hot fix");
+        System.out.println("push");
     }
 }
